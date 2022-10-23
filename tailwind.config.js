d393c1600264157c677c749fc13eb930b71f0f9d
archfiend-Tanvir <tanvirelhussain@gmail.com>
2022-10-23 19:17:06 +0100

    Initial Commit
    I created repository and added the needed file.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'], // this is telling the computer "to look in the build file and take all the html files"
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
